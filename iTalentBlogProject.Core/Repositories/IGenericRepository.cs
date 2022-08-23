483ccc5faa8dee2959aa3d4a0dca9b18eed474ac
Afevkaya <Afevkaya5151@gmail.com>
2022-08-24 01:13:25 +0300

    Added to project IGenericRepository interface.

---
+++ iTalentBlogProject.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTalentBlogProject.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(int id);
+
+    }
+}
