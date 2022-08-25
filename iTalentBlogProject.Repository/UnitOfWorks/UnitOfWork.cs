9b9f8b72c7d1751fa5606227b126c3015bc56658
Afevkaya <Afevkaya5151@gmail.com>
2022-08-25 18:40:14 +0300

    Repository Layer: Coding UnitOfWork class.

---
+++ iTalentBlogProject.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using iTalentBlogProject.Core.UnitOfWorks;
+using iTalentBlogProject.Repository.Context;
+
+namespace iTalentBlogProject.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly iTalentBlogContext _context;
+
+        public UnitOfWork(iTalentBlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
