e5ad419f7b09c998e5345736fa92ae60e99da47c
Afevkaya <Afevkaya5151@gmail.com>
2022-08-25 18:39:46 +0300

    Repository Layer: Coding repository classes.

---
+++ iTalentBlogProject.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using iTalentBlogProject.Core.Repositories;
+using iTalentBlogProject.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace iTalentBlogProject.Repository.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity: class
+    {
+        protected readonly iTalentBlogContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public GenericRepository(iTalentBlogContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbSet.AsNoTracking().AsQueryable();
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.AnyAsync(predicate);
+
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity != null)
+                _context.Entry(entity).State = EntityState.Detached;
+            return entity;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public  void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public async void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
---
+++ iTalentBlogProject.Repository/Repositories/PostRepository.cs
@@ -0,0 +1,13 @@
+﻿using iTalentBlogProject.Core.Entities;
+using iTalentBlogProject.Core.Repositories;
+using iTalentBlogProject.Repository.Context;
+
+namespace iTalentBlogProject.Repository.Repositories
+{
+    public class PostRepository : GenericRepository<Post>, IPostRepository
+    {
+        public PostRepository(iTalentBlogContext context) : base(context)
+        {
+        }
+    }
+}
