efbd06cd01b342fb9de049284dbe0b3efd6c0ffd
Afevkaya <Afevkaya5151@gmail.com>
2022-08-25 18:38:57 +0300

    Repository Layer: Added context class to the project.

---
+++ iTalentBlogProject.Repository/Context/iTalentBlogContext.cs
@@ -0,0 +1,16 @@
+﻿using iTalentBlogProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace iTalentBlogProject.Repository.Context
+{
+    public class iTalentBlogContext : DbContext
+    {
+        public iTalentBlogContext(DbContextOptions<iTalentBlogContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
