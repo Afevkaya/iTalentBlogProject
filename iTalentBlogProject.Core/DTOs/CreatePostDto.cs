26e391b339dfd8b9e965896f92a7917a08ccc71e
Afevkaya <Afevkaya5151@gmail.com>
2022-08-26 00:44:09 +0300

    Core Layer: Added DTOs classes to the project.

---
+++ iTalentBlogProject.Core/DTOs/CreatePostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTalentBlogProject.Core.DTOs
+{
+    public class CreatePostDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentCaption { get; set; }
+        public string PhotoUrl { get; set; }
+        public int CategoryId { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
