b7050e019e930050f8ba8e69f8a6a9234d579cd8
Afevkaya <Afevkaya5151@gmail.com>
2022-08-26 00:51:13 +0300

    Service Layer: Added MapProfile classes to the project.

---
+++ iTalentBlogProject.Services/Mapping/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using iTalentBlogProject.Core.DTOs;
+using iTalentBlogProject.Core.Entities;
+
+namespace iTalentBlogProject.Services.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<CreatePostDto, Post>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap();
+        }
+    }
+}
