4c0039d0eba2cf9c3d03a887a69d92ec0d068943
Afevkaya <Afevkaya5151@gmail.com>
2022-08-25 18:48:11 +0300

    Core Layer: Coding IGenericService interface.

---
+++ iTalentBlogProject.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTalentBlogProject.Core.Services
+{
+    public interface IGenericService<TDto> where TDto : class
+    {
+        Task<IEnumerable<TDto>> GetAllAsync();
+        IQueryable<TDto> Where(Expression<Func<TDto, bool>> predicate);
+        Task<bool> AnyAsync(Expression<Func<TDto, bool>> predicate);
+        Task<TDto> GetByIdAsync(int id);
+        Task AddAsync(TDto entity);
+        Task UpdateAsync(TDto entity);
+        Task RemoveAsync(TDto entity);
+    }
+}
