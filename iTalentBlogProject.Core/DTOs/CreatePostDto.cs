a1c2d84be4cf2288c74031302377965260dd646e
Afevkaya <Afevkaya5151@gmail.com>
2022-08-26 17:18:18 +0300

    Core Layer: Added CreateCommentDto class and updated CreatePostDto class.

--- iTalentBlogProject.Core/DTOs/CreatePostDto.cs
+++ iTalentBlogProject.Core/DTOs/CreatePostDto.cs
@@ -8,9 +8,8 @@ namespace iTalentBlogProject.Core.DTOs
 {
     public class CreatePostDto
     {
-        public int Id { get; set; }
         public string Title { get; set; }
-        public string ContentCaption { get; set; }
+        public string Article { get; set; }
         public string PhotoUrl { get; set; }
         public int CategoryId { get; set; }
         public CategoryDto CategoryDto { get; set; }
