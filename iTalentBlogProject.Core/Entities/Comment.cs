b11f8b4ece14a66063f704911079f79b7c592239
Afevkaya <Afevkaya5151@gmail.com>
2022-08-26 17:17:18 +0300

    Core Layer: Added comment class and updated Post class.

---
+++ iTalentBlogProject.Core/Entities/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTalentBlogProject.Core.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public string UserName { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+    }
+}
--- iTalentBlogProject.Core/Entities/Post.cs
+++ iTalentBlogProject.Core/Entities/Post.cs
@@ -4,11 +4,13 @@
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string ContentCaption { get; set; }
+        public string Article { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string PhotoUrl { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
 
     }
 }
