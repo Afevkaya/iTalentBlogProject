e70e6b071b446a3e6cfa9cb88f4df1e5181c03bc
Afevkaya <Afevkaya5151@gmail.com>
2022-08-26 17:19:50 +0300

    Repository Layer: Created add_comment migration.

--- iTalentBlogProject.Repository/Migrations/20220825223725_add_comment_class.Designer.cs
+++ iTalentBlogProject.Repository/Migrations/20220825223725_add_comment_class.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using iTalentBlogProject.Repository.Context;
 
@@ -11,9 +12,10 @@ using iTalentBlogProject.Repository.Context;
 namespace iTalentBlogProject.Repository.Migrations
 {
     [DbContext(typeof(iTalentBlogContext))]
-    partial class iTalentBlogContextModelSnapshot : ModelSnapshot
+    [Migration("20220825223725_add_comment_class")]
+    partial class add_comment_class
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
