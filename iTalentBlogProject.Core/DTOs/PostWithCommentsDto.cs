f765ba0152d91e99e6fea1082bf80a96e331770d
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 00:51:18 +0300

    Core Layer: Adding PostWithCommentDto class.

---
+++ iTalentBlogProject.Core/DTOs/PostWithCommentsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTalentBlogProject.Core.DTOs
+{
+    public class PostWithCommentsDto
+    {
+        public PostDto PostDto { get; set; }
+        public List<CommentDto> CommentDtos { get; set; }
+    }
+}
