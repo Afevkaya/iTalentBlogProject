812c0cbf7d2e76ff08d92c6b39e3e09410fd5cac
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 00:43:00 +0300

    Api Layer: Coding Save endpoint to comment class.

---
+++ iTalentBlogProject.API/Controllers/CommentsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using iTalentBlogProject.Core.DTOs;
+using iTalentBlogProject.Core.Entities;
+using iTalentBlogProject.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTalentBlogProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        private readonly IMapper _mapper;
+
+        public CommentsController(ICommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(CreateCommentDto createCommentDto)
+        {
+            var comment = _mapper.Map<Comment>(createCommentDto);
+            await _commentService.AddAsync(comment);
+            var commentDto = _mapper.Map<CommentDto>(comment);
+            return new ObjectResult(CustomResponseDto<CommentDto>.Success(commentDto, 201)) {StatusCode = 201};
+        }
+    }
+}
