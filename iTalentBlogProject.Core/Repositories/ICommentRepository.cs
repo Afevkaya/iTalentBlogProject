665a42303ce84493d8c4b9a770406b560441b389
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 00:45:47 +0300

    Core Layer: Creating ICommentRepositoy interface.

---
+++ iTalentBlogProject.Core/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTalentBlogProject.Core.Entities;
+
+namespace iTalentBlogProject.Core.Repositories
+{
+    public interface ICommentRepository : IGenericRepository<Comment>
+    {
+        
+    }
+}
