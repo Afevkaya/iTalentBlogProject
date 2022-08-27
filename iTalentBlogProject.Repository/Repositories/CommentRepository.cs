57923298e58d9c5562c453a6acd22f4905cf3fab
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 00:47:08 +0300

    Repositoy Layer: Creating CommentRepository class.

---
+++ iTalentBlogProject.Repository/Repositories/CommentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTalentBlogProject.Core.Entities;
+using iTalentBlogProject.Core.Repositories;
+using iTalentBlogProject.Repository.Context;
+
+namespace iTalentBlogProject.Repository.Repositories
+{
+    public class CommentRepository: GenericRepository<Comment>, ICommentRepository 
+    {
+        public CommentRepository(iTalentBlogContext context) : base(context)
+        {
+        }
+    }
+}
