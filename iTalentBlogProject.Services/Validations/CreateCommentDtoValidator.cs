b0b20d039c71bedc14284772922c32e748f954df
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 16:22:32 +0300

    Service Layer: Coding CreateDtoValidator classes.

---
+++ iTalentBlogProject.Services/Validations/CreateCommentDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using iTalentBlogProject.Core.DTOs;
+
+namespace iTalentBlogProject.Services.Validations
+{
+    public class CreateCommentDtoValidator : AbstractValidator<CreateCommentDto>
+    {
+        public CreateCommentDtoValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotNull().WithMessage("{PropertyName} null olamaz")
+                .NotEmpty().WithMessage("{PropertyName} boş olamaz")
+                .MinimumLength(2).WithMessage("{PropertyName} en az 2 karakter olmalıdır")
+                .MaximumLength(25).WithMessage("{PropertyName} ez az 50 karakter olmalıdır");
+
+            RuleFor(x => x.Text)
+                .NotNull().WithMessage("{PropertyName} null olamaz")
+                .NotEmpty().WithMessage("{PropertyName} boş olamaz")
+                .MinimumLength(20).WithMessage("{PropertyName} en az 2 karakter içermelidir")
+                .MaximumLength(200).WithMessage("{PropertyName} ez az 200 karakter içermelidir.");
+        }
+    }
+}
---
+++ iTalentBlogProject.Services/Validations/CreatePostDtoValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using iTalentBlogProject.Core.DTOs;
+
+namespace iTalentBlogProject.Services.Validations
+{
+    public class CreatePostDtoValidator: AbstractValidator<CreatePostDto>
+    {
+        public CreatePostDtoValidator()
+        {
+            RuleFor(x => x.Article)
+                .NotNull().WithMessage("{PropertyName} null olamaz")
+                .NotEmpty().WithMessage("{PropertyName} boş olamaz")
+                .MinimumLength(50).WithMessage("{PropertyName} en az 50 karakter içermelidir");
+            
+            RuleFor(x => x.PhotoUrl)
+                .NotNull().WithMessage("{PropertyName} null olamaz")
+                .NotEmpty().WithMessage("{PropertyName} boş olamaz");
+            
+            RuleFor(x=>x.Title)
+                .NotNull().WithMessage("{PropertyName} null olamaz")
+                .NotEmpty().WithMessage("{PropertyName} boş olamaz")
+                .MinimumLength(5).WithMessage("{PropertyName} en az 5 karakter içermelidir")
+                .MaximumLength(50).WithMessage("{PropertyName} en fazla 50 karakter içermelidir"); ;
+        }
+    }
+}
