8a6b85a65dc9e6a8131c626e3806b76dc01980a1
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 13:06:28 +0300

    UI Layer: Adding the UI layer to the project.

---
+++ iTalentBlogProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iTalentBlogProject.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
