076d5a928250c7583fac5f214540e2a62b4c0856
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 13:53:28 +0300

    Api Layer: Added CustomeBaseController and Coding.

---
+++ iTalentBlogProject.API/Controllers/CustomeBaseController.cs
@@ -0,0 +1,25 @@
+﻿using iTalentBlogProject.Core.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTalentBlogProject.API.Controllers
+{
+    /// <summary>
+    /// StatusCode'a göre geri dönüş tipi üreten bir Controller oluşturdum.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomeBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult CreateActionResult<T>(CustomeResponseDto<T> response)
+        {
+            if (response.StatusCode == 204)
+            {
+                return new ObjectResult(null) {StatusCode = response.StatusCode};
+            }
+
+            return new ObjectResult(response) {StatusCode = response.StatusCode};
+        }
+    }
+}
