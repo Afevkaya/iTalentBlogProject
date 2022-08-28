10273bbe636b6106988d69f57789fd65d62859bd
Afevkaya <Afevkaya5151@gmail.com>
2022-08-28 16:23:06 +0300

    Api Layer: Coding ValidateFilterAttribute filter class.

---
+++ iTalentBlogProject.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,19 @@
+﻿using iTalentBlogProject.Core.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace iTalentBlogProject.API.Filters
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+                context.Result = new BadRequestObjectResult(CustomeResponseDto<NoContentDto>.Fail(errors, 400));
+
+            }
+        }
+    }
+}
